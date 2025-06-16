2ac614b9e8ac484348bab875142b7c53a0dbdd8f
Timdev0x <tim.dev2019@gmail.com>
2025-06-16 12:28:55 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Newtonsoft.Json;
+
+class Program
+{
+    static void Main()
+    {
+        var person = new { Name = "John", Age = 30 };
+        string json = JsonConvert.SerializeObject(person);
+        Console.WriteLine(json);
+    }
+}
